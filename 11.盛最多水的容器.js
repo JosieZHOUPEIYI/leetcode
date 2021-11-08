de6d829206e7919665eb0bc8d8c18c0b30cd4644
josie <406271072@qq.com>
2021-11-08 10:50:36 +0800

    save

--- 11.盛最多水的容器.js
+++ 11.盛最多水的容器.js
@@ -7,23 +7,24 @@
 // @lc code=start
 /**
  * @param {number[]} height
+ *
  * @return {number}
  */
-var maxArea = function(height) {
+var maxArea = function (height) {
     //思路
     //从两边开始遍历，这是len最长，计算max，maxArea = len * min（height[left],height[right]）
     //直到指针重遇
-    let [left,right,max] = [0,height.length-1,0]
-    if(height.length < 2) return 0
-    while(left<right){
+    let [left, right, max] = [0, height.length - 1, 0]
+    if (height.length < 2) return 0
+    while (left < right) {
         const len = right - left
-        const area = Math.min(height[left],height[right]) * len
-        if(height[left] >= height[right]){
+        const area = Math.min(height[left], height[right]) * len
+        if (height[left] >= height[right]) {
             right--
-        }else{
+        } else {
             left++
         }
-        if(max < area){
+        if (max < area) {
             max = area
         }
     }
@@ -31,4 +32,4 @@ var maxArea = function(height) {
 };
 // @lc code=end
 
-console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
